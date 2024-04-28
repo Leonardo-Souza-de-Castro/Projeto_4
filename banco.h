e555b51982b2fe755e4c98f589d53cc9aac24018
Kayna2D <unifkaysilva@fei.edu.br>
2024-04-28 02:02:53 -0300

    alguns possiveis erros enumerados

--- banco.h
+++ banco.h
@@ -7,3 +7,19 @@ typedef struct Cliente {
     float saldo;
     char senha[100];
 } Cliente;
+
+typedef enum Erros {
+    OK,
+    SALDO_INSUFICIENTE,
+    NAO_ENCONTRADO,
+    SENHA_INVALIDA,
+    CPF_INVALIDO,
+    TIPO_CONTA_INVALIDO,
+    SEM_CLIENTES,
+    ESCREVER,
+    LER,
+    ABRIR,
+    FECHAR,
+    SEM_TRANSACOES,
+    MAX_CLIENTES,
+} Erro;
