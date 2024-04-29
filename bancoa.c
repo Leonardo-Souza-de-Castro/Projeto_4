51809fa5fdcbc8b827906c105f5a75f937d26239
Kayna2d <65991310+Kayna2D@users.noreply.github.com>
2024-04-29 22:32:16 +0000

    funcao criar criada

--- bancoa.c
+++ bancoa.c
@@ -3,7 +3,38 @@
 #include <string.h>
 
 Erro criar(Cliente contas[], int *pos) {
-    printf("Funcao criar\n");
+    // if (*pos >= TOTAL) {
+    //     return MAX_CLIENTES;
+    // }
+
+    printf("Digite seu nome: ");
+    scanf("%[^\n]", contas[*pos].nome);
+    clearBuffer();
+
+    printf("Digite seu CPF (apenas numeros): ");
+    scanf("%[^\n]", contas[*pos].cpf);
+    clearBuffer();
+    // if (strlen(contas[(*pos)].cpf) != 11)
+    //   return CPF_INVALIDO;
+  
+    printf("Digite o tipo de conta (comum ou plus): ");
+    scanf("%[^\n]", contas[*pos].tipo_conta);
+    clearBuffer();
+    // if (strcmp(contas[(*pos)].tipo_conta, "comum") != 0 || strcmp(contas[(*pos)].tipo_conta, "plus") != 0)
+    //   return TIPO_CONTA_INVALIDO;
+
+    printf("Digite seu saldo: ");
+    scanf("%f", &contas[*pos].saldo);
+    clearBuffer();
+
+    printf("Digite sua senha: ");
+    scanf("%[^\n]", contas[*pos].senha);
+    clearBuffer();
+
+    *pos = *pos + 1;
+
+
+  
     return OK;
 }
 
