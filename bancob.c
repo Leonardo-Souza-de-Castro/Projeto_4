4594e64afd5317370e8c2a35b75095b056b2572a
LEONARDO SOUZA DE CASTRO <uniflcastro@feinet.fei.edu.br>
2024-04-29 20:21:54 -0300

    função de deposito inicia falta testar

--- bancob.c
+++ bancob.c
@@ -8,7 +8,46 @@ Erro debitar(Cliente contas[], int *pos) {
 }
 
 Erro depositar(Cliente contas[], int *pos) {
-    printf("Funcao deposito\n");
+
+    char CPF[12];
+    float valor_depos;
+    int contador_erro = 0;
+
+    printf("Insira o CPF que irá receber este depósito: \n");
+    fgets(CPF, 12, stdin);
+    printf("Insira o valor que gostaria de depositar para esta conta \n");
+    scanf("%d", &valor_depos);
+
+    while (strlen(CPF) != 12)
+    {
+        printf("Insira um valor valido para CPF \n");
+        fgets(CPF, 12, stdin);
+    }
+
+    while (valor_depos <= 0)
+    {
+        printf("Insira um valor valido para depósito (maior que 0) \n");
+        scanf("%d", &valor_depos);
+    }
+    
+    for (int i = 0; i < *pos; i++)
+    {
+        if (strcmp(contas[i].cpf, CPF) == 0)
+        {
+            contas[i].saldo = contas[i].saldo + valor_depos;
+        }
+        else
+        {
+            contador_erro++;
+        }
+        
+    }
+
+    if (contador_erro >= pos)
+    {
+        return NAO_ENCONTRADO;
+    }
+
     return OK;
 }
 
