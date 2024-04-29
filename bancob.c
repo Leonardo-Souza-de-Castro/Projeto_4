c0c04800bb4253e21ac919cb2fd15b1c70516dd0
DANIEL SANTOS SOBRINHO <unifdsobrinho@feinet.fei.edu.br>
2024-04-29 20:40:15 -0300

    salvar

--- bancob.c
+++ bancob.c
@@ -16,3 +16,25 @@ Erro transferir(Cliente contas[], int *pos) {
     printf("Funcao transferencia\n");
     return OK;
 }
+
+Erro salvar(Cliente contas[], int *pos){
+    FILE *f  = fopen("contas.bin", "wb");
+
+    if (F == NULL) {
+        printf("Erro ao salvar o arquivo\n");
+        return ABRIR;
+    }
+
+    int num = fwrite(contas, TOTAL, sizeof(Cliente), f);
+    if (num == 0) {
+        printf("Erro para escrever");
+        return ESCREVER;
+    }
+
+    num = fwrite(pos, 1, sizeof(int), f);
+    if (num == 0) {
+        
+    }
+
+    return OK;
+}
