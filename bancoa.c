ab6e1c12e60132aa30d1528f1113e749731a5a3e
Kayna2d <65991310+Kayna2D@users.noreply.github.com>
2024-04-29 22:50:36 +0000

    erros testados e tratados localmente

--- bancoa.c
+++ bancoa.c
@@ -3,25 +3,32 @@
 #include <string.h>
 
 Erro criar(Cliente contas[], int *pos) {
-    // if (*pos >= TOTAL) {
-    //     return MAX_CLIENTES;
-    // }
+    if (*pos >= TOTAL) {
+        return MAX_CLIENTES;
+    }
+
+    char cpf[12];
 
     printf("Digite seu nome: ");
     scanf("%[^\n]", contas[*pos].nome);
     clearBuffer();
 
-    printf("Digite seu CPF (apenas numeros): ");
-    scanf("%[^\n]", contas[*pos].cpf);
-    clearBuffer();
-    // if (strlen(contas[(*pos)].cpf) != 11)
-    //   return CPF_INVALIDO;
-  
-    printf("Digite o tipo de conta (comum ou plus): ");
-    scanf("%[^\n]", contas[*pos].tipo_conta);
-    clearBuffer();
-    // if (strcmp(contas[(*pos)].tipo_conta, "comum") != 0 || strcmp(contas[(*pos)].tipo_conta, "plus") != 0)
-    //   return TIPO_CONTA_INVALIDO;
+    while (strlen(contas[(*pos)].cpf) != 11){
+      printf("Digite seu CPF (apenas numeros): ");
+      scanf("%[^\n]", contas[*pos].cpf);
+      clearBuffer();
+      if (strlen(contas[(*pos)].cpf) != 11)
+        printf("CPF invalido.\n");
+    }
+
+    while (strcmp(contas[(*pos)].tipo_conta, "comum") != 0 && strcmp(contas[(*pos)].tipo_conta, "plus") != 0){
+      printf("Digite o tipo de conta (comum ou plus): ");
+      scanf("%[^\n]", contas[*pos].tipo_conta);
+      clearBuffer();
+      if (strcmp(contas[(*pos)].tipo_conta, "comum") != 0 && strcmp(contas[(*pos)].tipo_conta, "plus") != 0)
+        printf("Tipo de conta invalido.\n");
+    }
+    
 
     printf("Digite seu saldo: ");
     scanf("%f", &contas[*pos].saldo);
