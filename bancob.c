1553f611f19036ce1b6a2b2092a0c7330d749f07
DANIEL SANTOS SOBRINHO <unifdsobrinho@feinet.fei.edu.br>
2024-05-10 20:14:08 -0300

    funcao carregar e listar finalizada e testada

--- bancob.c
+++ bancob.c
@@ -21,66 +21,56 @@ Erro transferir(Cliente contas[], int *pos) {
 }
 
 
-Erro salvar(Cliente contas[], int *pos){
-    FILE *f  = fopen("contas.bin", "wb");
+Erro salvar(Cliente contas[], int *pos) {
+  FILE *f = fopen("contas.bin", "wb");
 
+  if (f == NULL) {
+    printf("Erro ao salvar o arquivo\n");
+    return ABRIR;
+  }
 
-    if (f == NULL) {
-        printf("Erro ao salvar o arquivo\n");
-        return ABRIR;
-    }
+  int num = fwrite(contas, TOTAL, sizeof(Cliente), f);
+  if (num == 0) {
+    printf("Erro para escrever");
+    return ESCREVER;
+  }
 
+  num = fwrite(pos, 1, sizeof(int), f);
+  if (num == 0) {
+    printf("Erro para escrever posição\n");
+    return ESCREVER;
+  }
 
-    int num = fwrite(contas, TOTAL, sizeof(Cliente), f);
-    if (num == 0) {
-        printf("Erro para escrever");
-        return ESCREVER;
-    }
+  if (fclose(f)) {
+    printf("Erro ao fechar\n");
+    return FECHAR;
+  }
 
-
-    num = fwrite(pos, 1, sizeof(int), f);
-    if (num == 0) {
-        printf("Erro para escrever posição\n");
-        return ESCREVER;
-    }
-
-
-    if (fclose (f)) {
-        printf("Erro ao fechar\n");
-        return FECHAR;
-    }
-
-
-    return OK;
+  return OK;
 }
 
 
-ERRO carregar(Cliente contas[], int *pos) {
-    FILE *f = fopen("tarefas.bin", "rb");
-    if (f == NULL) {
-        printf("Erro ao abrir o arquivo\n");
-        return ABRIR;
-    }
+Erro carregar(Cliente contas[], int *pos) {
+  FILE *f = fopen("contas.bin", "rb");
+  if (f == NULL) {
+    printf("Erro ao abrir o arquivo\n");
+    return ABRIR;
+  }
 
-
-    int num = fread(contas, TOTAL, sizeof(Cliente), F);
-    if (num == 0) {
+  int num = fread(contas, TOTAL, sizeof(Cliente), f);
+  if (num == 0) {
     printf("Erro ao carregar arquivo\n");
     return LER;
-    }
+  }
 
+  num = fread(pos, 1, sizeof(int), f);
+  if (num == 0) {
+    printf("Erro para abrir posição de arquivo\n");
+    return LER;
+  }
 
-    num = fread(pos, 1, sizeof(int), f);
-    if (num == 0) {
-        printf("Erro para abrir posição de arquivo\n");
-        return LER;
-    }
-
-
-    if (fclose(f)) {
-        printf("Erro ao fechar arquivo\n");
-        return FECHAR;
-    }
-
-
+  if (fclose(f)) {
+    printf("Erro ao fechar arquivo\n");
+    return FECHAR;
+  }
 }
